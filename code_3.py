97c81f1fd72bfd0b80d52b076b5f5af368dd9d1e
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-12-26 18:02:49 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,59 @@
+def main():
+import random
+if __name__ == "__main__":
+    return data
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+
+    for item in data:
+
+
+def generate_random_data():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+
+def generate_random_data():
+
+    data = generate_random_data()
+def main():
+
+    main()
+    return data
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    const data = generateRandomData();
+    const data = [];
+    data.forEach(item => {
+        console.log("Random Number: " + item);
+    }
+    return data;
+main();
+}
+    for (let i = 0; i < 10; i++) {
+    });
+function main() {
+
+
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    }
+    data.forEach(item => {
+}
+
+    return data;
+function main() {
+    const data = [];
+    for (let i = 0; i < 10; i++) {
+
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+        console.log("Random Number: " + item);
+main();
+    });
+    const data = generateRandomData();
